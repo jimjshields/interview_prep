46bffa35791ff81c78bfeea3e4cacdba4e9fd1aa
jimjshields <jim.j.shields@gmail.com>
2015-02-20 18:15:02 -0500

    Added some practice.

--- practice/1_linked_lists.py
+++ practice/1_linked_lists.py
@@ -2,17 +2,177 @@
 # linked list: an object with a pointer to the first item in the list,
 # each successive item holds a pointer to the next item and some payload/data
 # the last item points to None
-# methods: add, remove, size, isEmpty, ???
+# methods: add, remove, size, isEmpty, search
+# two classes - Node and UnorderedLinkedList
 
-class LinkedList(object):
-	"""Represents an unordered linked list ADT."""
+### Issues - forgot certain functionality (search, append, pop, index, insert; get_data, set_data)
+### 	   - often forget 'self' as an argument
+### 	   - traversing the list gets confusing - make sure I account for empty list and always have current/previous nodes
+
+class Node(object):
+	"""Represents a node in a linked list. Has data and a pointer to the next node."""
+
+	def __init__(self, data, next=None):
+		"""Initializes a node."""
+
+		self.data = data
+		self.next = next
+
+	def get_next(self):
+		"""Returns the next node in the linked list."""
+
+		return self.next
+
+	def set_next(self, node):
+		"""Sets the next node in the linked list."""
+
+		self.next = node
+
+	def get_data(self):
+		"""Returns the data of the given node."""
+
+		return self.data
+
+	def set_data(self, new_data):
+		"""Sets the data of the given node."""
+
+		self.data = new_data
+
+class UnorderedLinkedList(object):
+	"""Represents an unordered linked list ADT. Assumes all data is unique."""
 
 	def __init__(self):
 		"""Initializes an empty linked list."""
 
 		self.head = None
 
-	def add(self, item):
-		"""Adds an item to the end of the list."""
+	def add(self, data):
+		"""Adds a node to the end of the list."""
 
-		
+		current = self.head
+		previous = None
+		node = Node(data)
+
+		while current != None:
+			previous = current
+			current = current.get_next()
+
+		if previous != None:
+			previous.set_next(node)
+		else:
+			self.head = node
+
+	def remove(self):
+		"""Removes the node at the end of the list."""
+
+		current = self.head
+		previous = None
+
+		while current != None:
+			previous = current
+			current = current.get_next()
+
+		if previous != None:
+			previous.set_next(None)
+		else:
+			self.head = None
+
+	def size(self):
+		"""Returns the size of the linked list."""
+
+		current = self.head
+		size = 0
+
+		while current != None:
+			size += 1
+			current = current.get_next()
+
+		return size
+
+	def is_empty(self):
+		"""Returns bool for whether the list is empty."""
+
+		return self.head == None
+
+	def search(self, data):
+		"""Returns bool for whether the given data is in the list."""
+
+		current = self.head
+		previous = None
+
+		while current != None and current.get_data() != data:
+			previous = current
+			current = current.get_next()
+
+		if current == None:
+			return False
+		else:
+			return True
+
+	def pop(self, index=None):
+		"""Removes and returns the item at the given index in the list.
+		   Default is None; if None, pop the last item."""
+
+		current = self.head
+		previous = None
+
+		if index == None:
+			while current.get_next() != None:
+				previous = current
+				current = current.get_next()
+
+			previous.set_next(None)
+			return current.get_data()
+		else:
+			count = 0
+			while current.get_next() != None and count < index:
+				previous = current
+				current = current.get_next()
+				count += 1
+
+			previous.set_next(current.get_next())
+			return current.get_data()
+
+	def index(self, data):
+		"""Returns the index number of the given data."""
+
+		count = 0
+		current = self.head
+
+		while current != None and current.get_data() != data:
+			current = current.get_next()
+			count += 1
+
+		if current.get_data() == data:
+			return count
+		else:
+			return False
+
+	def print_data(self):
+		"""Prints every item in the list."""
+
+		current = self.head
+
+		while current != None:
+			print current.get_data()
+			current = current.get_next()
+
+
+### Manual testing
+
+new_list = UnorderedLinkedList()
+print "{} should be True.".format(new_list.is_empty())
+new_list.add(1)
+print "{} should be False.".format(new_list.is_empty())
+print "{} should be 1.".format(new_list.size())
+new_list.add(10)
+new_list.add(14)
+new_list.add(15)
+new_list.add(20)
+print "{} should be 5.".format(new_list.size())
+print "{} should be True.".format(new_list.search(10))
+print "{} should be False.".format(new_list.search(5))
+print new_list.pop()
+new_list.pop(2)
+new_list.print_data()
+print new_list.index(10)
---
+++ practice/2_breadth_first_search.py
@@ -0,0 +1,100 @@
+# Implement a breadth-first search
+# Assume that a graph and queue ADT exist
+
+def breadth_first_search(graph, start):
+	"""Traverses a graph breadth-first - level by level.
+	   Does this using a queue to store vertices to traverse
+	   then by coloring visited vertices gray when they're first visited,
+	   and black when all of their children have been visited."""
+
+	# should be 0, not 1
+	start.set_dist(0)
+	# forgot this
+	start.set_pred(None)
+	
+	# don't set color yet
+	
+	vertex_queue = Queue()
+	vertex_queue.enqueue(start)
+
+	while not vertex_queue.is_empty():
+		current_vertex = vertex_queue.dequeue()
+		nbrs = current_vertex.get_connections()
+		for nbr in nbrs:
+			# need to check whether the nbr has been checked - forgot this
+			if nbr.get_color() == 'white':
+				
+				# do these three first, then enqueue
+				nbr.set_dist(1 + nbr.get_pred().get_dist())
+				nbr.color('gray')
+				# forgot this
+				nbr.set_pred(current_vertex)
+
+				vertex_queue.enqueue(nbr)
+		current_vertex.color('black')
+
+def traverse(y):
+	"""Traverese a graph from vertex y."""
+
+	x = y
+	while x.get_pred():
+		print x.get_data()
+		x = x.get_pred()
+	print x.get_data()
+
+
+# Actually implement the graph and vertex classes with those methods
+
+class Vertex(object):
+	"""Represents a vertex in a graph."""
+
+	def __init__(self, data):
+		"""Starts w/ some given data."""
+
+		self.data = data
+
+	def get_data(self):
+		"""Returns the data of the current vertex."""
+
+		return self.data
+
+	def set_pred(self, pred):
+		"""Sets the predecessor vertex for the current vertex. Can only have one."""
+
+		self.pred = pred
+
+	def get_pred(self):
+		"""Returns the predecessor vertex for the current vertex."""
+
+		return self.pred
+
+	def set_color(self, color):
+		"""Set the vertex to a color indicated whether it's been searched: white, gray, or black."""
+
+		self.color = color
+
+	def get_color(self):
+		"""Get the color of the current vertex."""
+
+		return self.color
+
+	def set_dist(self, dist):
+		"""Set the distance from the start of the current vertex."""
+
+		self.dist = dist
+
+	def get_dist(self):
+		"""Get the distance from the start of the current vertex."""
+
+		return self.dist
+
+class Graph(object):
+	"""Represents a graph ADT."""
+
+	def __init__(self):
+		"""Initializes a graph object."""
+
+		self.head = None
+
+	def add_vertex(self, vertex):
+		"""Adds a vertex to the graph."""
